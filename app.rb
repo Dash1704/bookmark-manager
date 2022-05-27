05c00b6dfb3ed35372b4b8f5ce9b2095dd366256
Dashiell Boyles <dashiellboyles@hotmail.com>
2022-05-27 11:03:55 +0100

    able to create new bookmarks

--- app.rb
+++ app.rb
@@ -7,14 +7,20 @@ class BookmarkManager < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  get '/' do
-    'Bookmark Manager'
-  end
-
   get '/bookmarks' do
     @bookmarks = Bookmark.all
-    erb :'bookmarks/index'
+    erb :"bookmarks/index"
   end
+
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
+  post '/bookmarks' do
+    Bookmark.create(url: params[:url])
+    redirect '/bookmarks'
+  end
+
 
   run! if app_file == $0
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,13 +1,11 @@
 feature 'Viewing bookmarks' do
-  scenario 'visiting the index page' do
-    visit('/')
-    expect(page).to have_content "Bookmark Manager"
-  end
-end
-
-feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
+    Bookmark.create(url: "http://www.makersacademy.com")
+    Bookmark.create(url: "http://www.destroyallsoftware.com")
+    Bookmark.create(url: "http://www.google.com")
+
     visit('/bookmarks')
+
     expect(page).to have_content "http://www.makersacademy.com"
     expect(page).to have_content "http://www.destroyallsoftware.com"
     expect(page).to have_content "http://www.google.com"
